b219baa29b0c781e7f23b6b26992b052ac3fddfd
Paulo Fegueredo <paulofegueredo@gmail.com>
2020-04-10 13:15:37 -0300

    Versão 3
    Com novos aplicativos adicionados

--- configure_station_centos7.sh
+++ configure_station_centos7.sh
@@ -1,8 +1,12 @@
 #Para ser usado no CentOS 7.0
 #!/bin/bash
 
-Atualiizar os pacotes do Cent Os
-#yum update -y
+#Atualizar os pacotes do CentOS
+yum update -y
+
+echo "Criando diretório para download de programas"
+mkdir /tmp/download
+cd /tmp/download
 
 #Instalar um repositorio que permite instalar aplicativos, entre eles o VLC:
 yum -y install http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
@@ -34,42 +38,55 @@ wget https://download.anydesk.com/linux/anydesk-5.5.1-1.el7.x86_64.rpm
 yum localinstall anydesk-5.5.1-1.el7.x86_64.rpm
 
 #Habilitar o reconhecimento de celular no linux
-yum install simple-mtpfs gvfs-mtp kio_mtp libmtp libmtp*
+yum -y install simple-mtpfs gvfs-mtp kio_mtp libmtp libmtp*
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 
 yum localinstall -y google-chrome-stable_current_x86_64.rpm
 
-#Nao esquecer de importar o Extension Packer do Virtual Box. 
+echo "Nao esquecer de importar o Extension Packer do Virtual Box." 
 #Comando para o virtualbox reconhecer a usb e outros recursos
 usermod -a -G vboxusers usuario
 
 #Instalar o Skype
 wget https://go.skype.com/skypeforlinux-64.rpm
+yum localinstall ./skypeforlinux-64.rpm
 
-yum install telnet -y
+#Instalar o cliente Telnet
+yum -y install telnet
 
-yum install python-pip python-wheel -y
+yum -y install python-pip python-wheel
 
-yum install python34u -y
+yum -y install python34u
 
 pip install --upgrade pip
 
-yum install python36
+yum -y install python36
 
-yum install whois -y
+yum -y install whois
 
-yum install curl -y
+yum -y install curl
 
-yum install build-essential curl zlib1g-dev git libfuse-dev vim autoconf automake autopoint libtool pkg-config flex byacc libcrypto++-dev testdisk tesseract-ocr tesseract-ocr-por tesseract-ocr-osd mplayer libparse-win32registry-perl libesedb-utils imagemagick libmsiecf-utils libafflib-dev libewf-dev libvmdk-dev libxtst6 libxi6 libpff1 openjfx -y
+#Instalar diversas bibliotecas
+yum -y install build-essential curl zlib1g-dev git libfuse-dev vim autoconf automake autopoint libtool pkg-config flex byacc libcrypto++-dev testdisk tesseract-ocr tesseract-ocr-por tesseract-ocr-osd mplayer libparse-win32registry-perl libesedb-utils imagemagick libmsiecf-utils libafflib-dev libewf-dev libvmdk-dev libxtst6 libxi6 libpff1 openjfx
 
-yum install aircrack-ng -y
+yum -y install aircrack-ng
 
-yum install wireshark -y
+#Instalar o Wireshark
+yum -y install wireshark
+yum -y install wireshark-gnome
 
 #Pacote com ferramentas de ataque (arpspoof, dnspoof, etc)
-yum install dsniff -y
+yum -y install dsniff
+
 echo 1 > /proc/sys/net/ipv4/ip_forward
-sudo yum install -y openssl-devel
-yum install openvpn -y
-yum install nikto -y
+
+yum -y install openssl-devel
+
+yum -y install openvpn
+
+yum -y install nikto
+
+yum -y install ftp
+
+
