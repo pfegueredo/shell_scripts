5f84307c25fbd1e9cc805d7b9985f7af364d4eae
Paulo Fegueredo <paulofegueredo@gmail.com>
2020-04-28 20:53:16 -0300

    Scaner de hosts ativos
    Varre a rede em busca de hosts ativos.

--- pingnet.sh
+++ pingnet.sh
@@ -1,7 +1,11 @@
 if [ "$1" == "" ] 
+
 then
+
 echo "Digite o nome do programa e a rede a ser escaneada."
 echo "Exemplo de Uso: $0 10.0.0" 
 else
+
 for ip in {1..254}; do ping -c1 $1.$ip | grep "64 bytes" | cut -d " " -f4 | cut -d ":" -f 1; done
+
 fi
